b97e103bb1cd9b3f1e15c5a93241fb22be4bc807
HoeksMJ <hoeksmj@gmail.com>
2023-05-19 10:45:52 -0500

    add number button functionality

--- script.js
+++ script.js
@@ -2,7 +2,100 @@ const displayNum = document.getElementById("displayNum");
 
 const clearDisplay = () => {displayNum.innerText = 0;};
 
-displayNum.innerText = "hi"
-
 const clearBtn = document.getElementById("clearBtn");
-clearBtn.addEventListener("click", () => {console.log("clicked"); clearDisplay();});
+clearBtn.addEventListener("click", () => {clearDisplay();});
+
+const zeroBtn = document.getElementById("zeroBtn");
+zeroBtn.addEventListener("click", () => {
+    displayNum.innerText = String(displayNum.innerText) + "0";
+});
+
+const oneBtn = document.getElementById("oneBtn");
+oneBtn.addEventListener("click", () => {
+    if (displayNum.innerText == 0){
+        displayNum.innerText = 1;
+    }
+    else {
+        displayNum.innerText = String(displayNum.innerText) + "1";
+    };
+});
+
+const twoBtn = document.getElementById("twoBtn");
+twoBtn.addEventListener("click", () => {
+    if (displayNum.innerText == 0){
+        displayNum.innerText = 2;
+    }
+    else {
+        displayNum.innerText = String(displayNum.innerText) + "2";
+    };
+});
+
+const threeBtn = document.getElementById("threeBtn");
+threeBtn.addEventListener("click", () => {
+    if (displayNum.innerText == 0){
+        displayNum.innerText = 3;
+    }
+    else {
+        displayNum.innerText = String(displayNum.innerText) + "3";
+    };
+});
+
+const fourBtn = document.getElementById("fourBtn");
+fourBtn.addEventListener("click", () => {
+    if (displayNum.innerText == 0){
+        displayNum.innerText = 4;
+    }
+    else {
+        displayNum.innerText = String(displayNum.innerText) + "4";
+    };
+});
+
+const fiveBtn = document.getElementById("fiveBtn");
+fiveBtn.addEventListener("click", () => {
+    if (displayNum.innerText == 0){
+        displayNum.innerText = 5;
+    }
+    else {
+        displayNum.innerText = String(displayNum.innerText) + "5";
+    };
+});
+
+const sixBtn = document.getElementById("sixBtn");
+sixBtn.addEventListener("click", () => {
+    if (displayNum.innerText == 0){
+        displayNum.innerText = 6;
+    }
+    else {
+        displayNum.innerText = String(displayNum.innerText) + "6";
+    };
+});
+
+const sevenBtn = document.getElementById("sevenBtn");
+sevenBtn.addEventListener("click", () => {
+    if (displayNum.innerText == 0){
+        displayNum.innerText = 7;
+    }
+    else {
+        displayNum.innerText = String(displayNum.innerText) + "7";
+    };
+});
+
+const eightBtn = document.getElementById("eightBtn");
+eightBtn.addEventListener("click", () => {
+    if (displayNum.innerText == 0){
+        displayNum.innerText = 8;
+    }
+    else {
+        displayNum.innerText = String(displayNum.innerText) + "8";
+    };
+});
+
+const nineBtn = document.getElementById("nineBtn");
+nineBtn.addEventListener("click", () => {
+    if (displayNum.innerText == 0){
+        displayNum.innerText = 9;
+    }
+    else {
+        displayNum.innerText = String(displayNum.innerText) + "9";
+    };
+});
